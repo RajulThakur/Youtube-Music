3310d53f9aa727172e8774588ee1d3ece4ab7031
YellowFlash <uchihasasuke9315446155@gmail.com>
2023-09-30 19:48:38 +0530

    adding some more rows

---
+++ data/data.js
@@ -0,0 +1,13 @@
+const songs=[
+    {
+        songid:'478324783839',
+        songName:"Likhe jo khat tujhe (KANYADAAN)",
+        songPublisher:"Mohammed Rafi",
+        views:"21M",
+        image:"images/imager/hq720 (7).jpg"
+    },
+    {
+        songid:'438438879',
+        songName:""
+    }
+]
